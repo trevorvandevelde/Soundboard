4b40c30e5a1de23593bdc5339f7c4d82fc50a94e
Trevor Van de Velde <vantrevor@berkeley.edu>
2021-10-28 08:35:02 -0400

    Firebase Upload Implemented

--- app/src/main/java/com/example/soundboard/MainActivity.kt
+++ app/src/main/java/com/example/soundboard/MainActivity.kt
@@ -1,13 +1,20 @@
 package com.example.soundboard
 
+import android.content.Intent
 import android.os.Bundle
-import android.support.design.widget.BottomNavigationView
-import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import android.widget.TextView
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.internal.ContextUtils.getActivity
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var textMessage: TextView
+
+    private lateinit var button: Button
+
     private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
@@ -29,9 +36,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
-        textMessage = findViewById(R.id.message)
-        navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+        button = findViewById(R.id.uploadAcitivtyButton)
+        button.setOnClickListener{
+            startUploadSoundByteActivity()
+        }
+
+
+
+
+        //val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+        //textMessage = findViewById(R.id.message)
+        //navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+    }
+
+    fun startUploadSoundByteActivity() {
+        val intent = Intent(this, UploadSoundByteActivity::class.java)
+        startActivity(intent)
     }
 }
