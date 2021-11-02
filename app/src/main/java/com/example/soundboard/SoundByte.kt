adbe7bacc3d93ca5aeeb746298a3e068a25f475c
Trevor Van de Velde <vantrevor@berkeley.edu>
2021-11-02 11:16:57 -0400

    updated SoundByte.kt

--- app/src/main/java/com/example/soundboard/SoundByte.kt
+++ app/src/main/java/com/example/soundboard/SoundByte.kt
@@ -5,17 +5,48 @@ class SoundByte {
     private lateinit var songName: String
     private lateinit var songUrl: String
     private lateinit var imageUrl: String
-    private lateinit var uploaderUserName : String
+    private lateinit var uploaderUserName: String
     private lateinit var id: String
     private lateinit var description: String
     private lateinit var tag: String
 
 
-
-    fun SoundByte(songName : String, songUrl : String, uploaderUserName: String){
+    fun SoundByte(songName: String, songUrl: String, uploaderUserName: String) {
         this.songName = songName
         this.songUrl = songUrl
         //this.imageUrl = imageUrl
+        this.uploaderUserName = uploaderUserName
+    }
+
+    fun getSongName(): String {
+        return songName
+    }
+
+    fun setSongName(songName: String) {
+        this.songName = songName
+    }
+
+    fun getSongUrl(): String {
+        return songUrl
+    }
+
+    fun setSongUrl(songUrl : String){
+        this.songUrl = songUrl
+    }
+
+    fun getImageUrl(): String {
+        return imageUrl
+    }
+
+    fun setImageUrl(imageUrl : String){
+        this.imageUrl = imageUrl
+    }
+
+    fun getUploaderUserName() : String {
+        return uploaderUserName
+    }
+
+    fun setUploaderUserName(uploaderUserName: String){
         this.uploaderUserName = uploaderUserName
     }
 }
